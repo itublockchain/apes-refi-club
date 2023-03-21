d977e4f1408b0c975f008000f8a912f983253206
koybasimuhittin <koybasimuhittin@gmail.com>
2023-03-21 14:37:04 +0300

    apes page by id

--- packages/frontend/components/ApeCard.tsx
+++ packages/frontend/components/ApeCard.tsx
@@ -1,20 +1,31 @@
-type ApeCardProps = {
-  ape: Ape;
-};
+import Link from 'next/link';
 
-type Ape = {
-  paidCarbonPercantage: number;
-  image: string;
+type ApeCardProps = {
+  id: string;
 };
 
 function ApeCard(props: ApeCardProps) {
-  const { ape } = props;
+  const { id } = props;
   return (
-    <>
-      <div className='w-40 h-40 bg-black rounded-md border-2 border-black'>
-        <img src={ape.image} alt='' />
+    <div className='h-screen w-screen'>
+      <div className='flex flex-col p-20'>
+        <div className='flex flex-row'>
+          <div className='w-96 h-96 rounded-md border-2 border-black'>
+            <div className='h-72'>
+              <img src={''} alt='' className='bg-black' />
+            </div>
+            <div className='h-24 flex items-center justify-center'>
+              <button className='rounded-lg border-none- shadow-md bg-black w-2/3 h-10'></button>
+            </div>
+          </div>
+          <div className='w-full h-96'>
+            <div className='flex flex-col px-10'>
+              <span className='text-lg font-bold py-10'>{`BAYC #${id}`}</span>
+            </div>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
