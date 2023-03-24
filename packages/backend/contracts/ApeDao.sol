4c77f2a92da8f24836cfadf823f8d653099964aa
koybasimuhittin <koybasimuhittin@gmail.com>
2023-03-24 08:58:00 +0300

    apeDAO update

--- packages/backend/contracts/ApeDao.sol
+++ packages/backend/contracts/ApeDao.sol
@@ -1,52 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-//import "contracts/scalingEth/erc721a.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // This contract demonstrates a basic DAO (Decentralized Autonomous Organization) system called ApeDao.
 
 contract ApeDao {
 
-    event NewProposal(uint indexed proposalIndex, address indexed proposalOwner, address to, bytes32 description, uint deadline);
-    event Voted(uint indexed proposalIndex, address indexed voter, uint votePower, bool vote);
-    event ProposalExecuted(uint indexed proposalIndex, address indexed executor, address to, uint totalFund);
-    event Refunded(uint indexed proposalIndex, address indexed refundee, uint refundedAmount);
+    event NewProposal(bytes32 proposalID, address to, uint deadline, uint requestedFund);
+    event Voted(bytes32 indexed proposalID, address indexed voter, uint votePower, bool vote);
+    event ProposalApproved(bytes32 indexed proposalID, address executor, address to, uint requestedFund);
+    event ProposalRejected(bytes32 indexed proposalID, address executor);
 
+    uint256 constant MINIMUM_REQUESTABLE = 10;
 
     // Defines the data structure of a proposal.
     struct Proposal{
-        address proposalOwner;
         uint yesVotes;
         uint noVotes;
         uint deadline;
         address to;
-        //string description;
-        bytes32 descriptionHash;
 
-        mapping(uint => bool)voters;
-        mapping(address => uint) addressToFundedAmount;
+        // proposal hash keccak256(toUtf8Bytes([name, slug, owner, description, website, endDate, createDate, desiredAmount, account].toString()));
+        bytes32 proposalID;
+
+        mapping(uint => bool)votes;
 
         bool executed;
-        uint totalFund;
+        uint requestedFund;
     }
 
-    mapping(uint => Proposal) proposals;
+    mapping(bytes32 => Proposal) public proposals;
     uint public numberOfProposal;
     uint public treasuryBalance;
+    uint public availableTreasury;
 
     // Defines the token smart contract (ERC721) according to which the proposals will be managed and provides a reference to it.
     IERC721 ARClubNFT;
-    IERC20 BARYCToken;
+    IERC20 ApeCoin;
 
     constructor(address _nftAddress, address _ApeCoinAddress){
-        BARYCToken = IERC20(_ApeCoinAddress);
+        ApeCoin = IERC20(_ApeCoinAddress);
         ARClubNFT = IERC721(_nftAddress);
     }
 
-    enum Vote {
-        yes,
-        no
-    }
 
     // An access modifier that ensures only ARClubNFT owners (APEDAO members) can perform the task.
     modifier ARCHolderOnly {
@@ -55,91 +52,77 @@ contract ApeDao {
     }
 
     // An access modifier that checks if the proposal is still active.
-    modifier activeProposal(uint _proposalIndex){
-        require(block.timestamp < proposals[_proposalIndex].deadline);
+    modifier activeProposal(bytes32 _proposalID){
+        require(block.timestamp < proposals[_proposalID].deadline);
         _;
     }
 
     // An access modifier that checks whether the proposal has been successful.
-    modifier successfulProposalOnly(uint _proposalIndex){
-        require(block.timestamp > proposals[_proposalIndex].deadline,"Wait for the deadline");
-        require(proposals[_proposalIndex].yesVotes > proposals[_proposalIndex].noVotes,"Proposal denied");
-        _;
-    }
-
-    // An access modifier that checks whether the proposal has been rejected.
-    modifier rejectedProposal(uint _proposalIndex){
-        require(block.timestamp > proposals[_proposalIndex].deadline,"Wait for the deadline");
-        require(proposals[_proposalIndex].yesVotes <= proposals[_proposalIndex].noVotes,"Successful proposal");
+    modifier finishedProposalOnly(bytes32 _proposalID){
+        require(block.timestamp > proposals[_proposalID].deadline,"Wait for the deadline");
         _;
     }
 
     // Creates a new proposal with the specified parameters.
-    // check archolderonly
-
     function receiveApeCoins(uint _amount) public {
-        require(BARYCToken.transferFrom(msg.sender, address(this), _amount), "Transfer failed");
+        require(ApeCoin.transferFrom(msg.sender, address(this), _amount), "Transfer failed");
         treasuryBalance += _amount;
+        availableTreasury += _amount;
     }
 
-    function createProposal(address _to, bytes32 _description) external ARCHolderOnly {
-        Proposal storage proposal = proposals[numberOfProposal];
-        proposal.proposalOwner = msg.sender;
+    function createProposal(address _to, bytes32 _proposalID, uint _requestedFund) external {
+        if(availableTreasury > MINIMUM_REQUESTABLE){
+            require(_requestedFund <= availableTreasury / 2, 'Requested amount is too high');
+        }
+        else {
+            require(_requestedFund <= availableTreasury, 'Requested amount is too high');
+        }
+        Proposal storage proposal = proposals[_proposalID];
         proposal.to = _to;
-        proposal.descriptionHash = _description;
+        proposal.proposalID = _proposalID;
         proposal.deadline = block.timestamp + 7 days;
-        emit NewProposal(numberOfProposal, msg.sender, _to, _description, proposal.deadline);
         numberOfProposal ++;
+        availableTreasury -= _requestedFund;
+        emit NewProposal(_proposalID, _to, proposal.deadline, _requestedFund);
     }
 
     // Cast votes for the proposal.
-    function voteTheProposal(Vote vote, uint proposalIndex, uint[] memory NFTsToVote) external payable ARCHolderOnly activeProposal(proposalIndex) {
+    function voteTheProposal(bool vote, bytes32 proposalID, uint[] memory NFTsToVote) external ARCHolderOnly activeProposal(proposalID) {
         uint votePover = NFTsToVote.length;
         require(votePover > 0,"You need some nft to gain vote power");
-        Proposal storage proposal = proposals[proposalIndex];
+        Proposal storage proposal = proposals[proposalID];
         for(uint i; i< votePover; i++){
             require(ARClubNFT.ownerOf(NFTsToVote[i]) == msg.sender,"You are not the owner");
-            require(!proposal.voters[NFTsToVote[i]],"Already voted");
-            proposal.voters[NFTsToVote[i]] = true;
+            require(!proposal.votes[NFTsToVote[i]],"Already voted");
+            proposal.votes[NFTsToVote[i]] = true;
         }
-        if(vote == Vote.yes){
+        if(vote){
             proposal.yesVotes += votePover;
-            proposal.addressToFundedAmount[msg.sender] += msg.value;
-            proposal.totalFund += msg.value;
         }
-        if(vote == Vote.no){
+        else{
             proposal.noVotes += votePover;
         }
-        emit Voted(proposalIndex, msg.sender, votePover, vote == Vote.yes);
+        emit Voted(proposalID, msg.sender, votePover, vote);
 
     }
 
-    // Executes the successful proposal and sends the funds.
-    // check archolderonly
-    function executeProposal(uint proposalIndex) external ARCHolderOnly successfulProposalOnly(proposalIndex) {
-        Proposal storage proposal = proposals[proposalIndex];
+    // Executes the proposal and sends the funds.
+    function executeProposal(bytes32 proposalID) external finishedProposalOnly(proposalID) {
+        Proposal storage proposal = proposals[proposalID];
         require(!proposal.executed,"Proposal is already executed");
-        proposal.executed = true;
-        (bool success,)= proposal.to.call{value: proposal.totalFund+treasuryBalance}("");
-        require(success,"Transfer failed");
-        proposal.totalFund = 0;
-        treasuryBalance = 0;
-
-        emit ProposalExecuted(proposalIndex, msg.sender, proposal.to, proposal.totalFund+treasuryBalance);
-
+        if(proposal.yesVotes > proposal.noVotes) {
+            bool success = ApeCoin.transfer(proposal.to, proposal.requestedFund);
+            require(success,"Transfer failed");
+            proposal.executed = true;
+            treasuryBalance -= proposal.requestedFund;
+            emit ProposalApproved(proposalID, msg.sender, proposal.to, proposal.requestedFund);
+        }
+        else{
+            availableTreasury += proposal.requestedFund;
+            emit ProposalRejected(proposalID, msg.sender);
+        }
     }
 
-    // Refunds the contributors of a rejected proposal.
-    function refund(uint proposalIndex) external ARCHolderOnly rejectedProposal(proposalIndex){
-        Proposal storage proposal = proposals[proposalIndex];
-        require(proposal.addressToFundedAmount[msg.sender] > 0,"You have not");
-        (bool success,)= proposal.to.call{value: proposal.addressToFundedAmount[msg.sender]}("");
-        require(success,"Transfer failed");
-        uint refundedAmount = proposal.addressToFundedAmount[msg.sender];
-        proposal.addressToFundedAmount[msg.sender] = 0;
-        emit Refunded(proposalIndex, msg.sender, refundedAmount);
-
-    }
 
     // Accepts incoming payments
     receive() external payable{}
